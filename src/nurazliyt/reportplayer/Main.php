7f4f7f6f3c896664d2443f466ac1c688500581be
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-02-09 22:47:05 +0700

    Create Main.php

---
+++ src/nurazliyt/reportplayer/Main.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace nurazliyt\reportplayer;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\form\Form;
+use pocketmine\playet\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener {
+
+    private $reportQueue = [];
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+    }
+
+    public function onPlayerInteract(PlayerInteractEvent $event) {
+        $player = $event->getPlayer();
+        $targetPlayer = $event->getBlock()->getLevel()->getBlock($event->getBlock())->getLevel()->getHighestBlockAt($event->getBlock());
+
+        // Implement logic to show report UI
+        $this->sendReportForm($player, $targetPlayer);
+    }
+
+    public function sendReportForm(Player $player, Player $targetPlayer) {
+        $form = $this->getServer()->getPluginManager()->getPlugin("FormAPI")->createCustomForm(function (Player $player, array $data = null) use ($targetPlayer) {
+            if ($data !== null) {
+                // Handle form response, e.g., add report to queue
+                $this->addToReportQueue($player, $targetPlayer, $data[1]);
+            }
+        });
+
+        $form->setTitle("Report Player");
+        $form->addLabel("You are reporting " . $targetPlayer->getName() . ". Please provide details below.");
+
+        // Add a text input for additional details
+        $form->addInput("Additional details (optional):");
+
+        $form->sendToPlayer($player);
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
+        if ($command->getName() === "report") {
+            if ($sender instanceof Player) {
+                if (count($args) === 1) {
+                    $reportedPlayer = $this->getServer()->getPlayer($args[0]);
+                    if ($reportedPlayer !== null && $reportedPlayer->isOnline()) {
+                        // Implement logic to show report UI
+                        $this->sendReportForm($sender, $reportedPlayer);
+                    } else {
+                        $sender->sendMessage(TextFormat::RED . "Player not found or not online.");
+                    }
+                } else {
+                    $sender->sendMessage(TextFormat::RED . "Usage: /report <player>");
+                }
+            } else {
+                $sender->sendMessage(TextFormat::RED . "This command can only be used in-game.");
+            }
+            return true;
+        } elseif ($command->getName() === "reportlist") {
+            // Display a list of pending reports to admins
+            $sender->sendMessage("Pending Reports:");
+            foreach ($this->reportQueue as $index => $report) {
+                $sender->sendMessage(($index + 1) . ". " . $report['reporter'] . " reported " . $report['reportedPlayer'] . ": " . $report['details']);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    // Metode lainnya seperti addToReportQueue, notifyAdmins, dan handleReport tetap tidak berubah.
+}
+// https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6https://goo.gl/maps/7rk2URiTepNA4RqC6
